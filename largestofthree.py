427babf355c7487ef741b03f912f7154aba31981
Devadharshinisubiksha <52270298+Devadharshinisubiksha@users.noreply.github.com>
2019-07-02 15:17:07 +0530

    Create largestofthree.py

---
+++ largestofthree.py
@@ -0,0 +1,10 @@
+n1 = 26
+n2 = 25
+n3 = 88
+if (n1 > n2) and (n1 > n3):
+   largest = n1
+elif (n2 > n1) and (n2 > n3):
+   largest = n2
+else:
+   largest = n3
+print("The largest number is",largest)
