c15723514c368ea80a44ed8bd6dac22b739a0e81
Devadharshinisubiksha <52270298+Devadharshinisubiksha@users.noreply.github.com>
2019-07-02 15:32:11 +0530

    Create leapornot.py

---
+++ leapornot.py
@@ -0,0 +1,11 @@
+ yr = int(input(" "))
+if (yr % 4) == 0:
+   if (yr % 100) == 0:
+       if (yr % 400) == 0:
+           print("{0} is a leap year".format(yr))
+       else:
+           print("{0} is not a leap year".format(yr))
+   else:
+       print("{0} is a leap year".format(yr))
+else:
+   print("{0} is not a leap year".format(yr))
