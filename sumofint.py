f3febf41b40f0ec410dada7f152f98733bcca128
Devadharshinisubiksha <52270298+Devadharshinisubiksha@users.noreply.github.com>
2019-07-03 14:47:56 +0530

    Update sumofint.py

--- sumofint.py
+++ sumofint.py
@@ -1,6 +1,6 @@
-b,c=map(int,input().split())
-d=list(map(int,input().split()))
-e=0
-for i in range(c):
-  e=e+d[i]
-print(e)
+a1,a2=map(int,input().split())
+a=list(map(int,input().split()))
+sum=0
+for i in range(a2):
+  sum=sum+a[i]
+print(sum)  
