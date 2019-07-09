ec768e0537cc030c56c0b518b9ea8dd9eb8b4b94
Devadharshinisubiksha <52270298+Devadharshinisubiksha@users.noreply.github.com>
2019-07-09 14:53:00 +0530

    Update primebetween.py

--- primebetween.py
+++ primebetween.py
@@ -1,7 +1,7 @@
-c,d=map(int,input().split())
-for i in range(c+1,d):
-  for j in range(2,i): 
-    if(i%j==0):
-      break
-  else:
-    print(i)
+s,t=map(int,input().split())
+for i in range(s+1,t):
+	for j in range(2,i):
+		if(i%j==0):
+			break
+	else:
+		print(i,end=" ")
