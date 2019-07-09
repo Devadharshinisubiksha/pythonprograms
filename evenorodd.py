2dc320df298d8d0fc56f253c7091dce3a1717719
Devadharshinisubiksha <52270298+Devadharshinisubiksha@users.noreply.github.com>
2019-07-09 13:58:05 +0530

    Update evenorodd.py

--- evenorodd.py
+++ evenorodd.py
@@ -1,5 +1,7 @@
-k=int(input("num "))
-if k%2==0:
-  print("e")
+c=int(input())
+if (c%2==0) & (c>0):
+        print("Even")
+elif (c%2!=0) & (c>0):
+        print("Odd")
 else:
-    print("o")
+    print("Invalid")
