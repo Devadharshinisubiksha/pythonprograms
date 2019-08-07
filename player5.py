a8d02f5706447c5d33ea107954c34f26f145e716
Devadharshinisubiksha <52270298+Devadharshinisubiksha@users.noreply.github.com>
2019-08-07 15:18:58 +0530

    Create player5.py

---
+++ player5.py
@@ -0,0 +1,7 @@
+list16=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+list26=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+up16=str(input())
+for k in range(20):
+  if(up16==list16[k]):
+    print(list26[k])
+    break
